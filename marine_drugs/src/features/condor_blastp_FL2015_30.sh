5cb9f3afc989cdfac05bf52f3c38d63f4c8605ac
WiscEvan <erees@wisc.edu>
2020-10-21 12:26:40 -0500

    :memo: Add condor_blastp executables and submission files :memo: Add condor_blastp templates for generation of executables and submission files :memo: Add script to generate condor_blastp_<sample> executables and submission files from templates.

---
+++ marine_drugs/src/features/condor_blastp_FL2015_30.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# First, copy the nr database from /staging into the working directory,
+cp /staging/groups/kwan_group/nr.dmnd ./
+
+./diamond blastp \
+    --query FL2015_30.orfs.faa \
+    --out FL2015_30.blastp.tsv \
+    --db nr.dmnd \
+    --evalue 1e-5 \
+    --max-target-seqs 200 \
+    --outfmt 6 \
+    --threads 16
+    --memory-limit 100
+
+#
+# Before the script exits, make sure to remove the file(s) from the working directory
+rm nr.dmnd
