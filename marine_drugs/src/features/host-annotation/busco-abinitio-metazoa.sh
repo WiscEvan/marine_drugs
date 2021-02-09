6ef030c8f54e29a8bf5103463ae5a60cf60e3a52
WiscEvan <erees@wisc.edu>
2021-02-09 10:04:58 -0600

    :art: Add more reference sponges to busco abinitio annotation

--- marine_drugs/src/features/host-annotation/busco-abinitio-metazoa.sh
+++ marine_drugs/src/features/host-annotation/busco-abinitio-metazoa.sh
@@ -44,17 +44,25 @@ done
 
 # Retrieve positive control from NCBI
 # cd $HOME/marine_drugs/marine_drugs/data/external/
+# Amphimedon Queenslandica
 # wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/090/795/GCF_000090795.1_v1.0/GCF_000090795.1_v1.0_genomic.fna.gz
 # gzip -d GCF_000090795.1_v1.0_genomic.fna.gz
-
 # wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/016/292/275/GCA_016292275.1_UQ_AmQuee_3/GCA_016292275.1_UQ_AmQuee_3_genomic.fna.gz
 # gzip -d GCA_016292275.1_UQ_AmQuee_3_genomic.fna.gz
+# Aplysina aerophoba
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/900/275/565/GCA_900275565.1_Aplysina16A_polished_assembly/GCA_900275565.1_Aplysina16A_polished_assembly_genomic.fna.gz
+# gzip -d GCA_900275565.1_Aplysina16A_polished_assembly_genomic.fna.gz
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/900/275/575/GCA_900275575.1_Aplysina21_polished_assembly/GCA_900275575.1_Aplysina21_polished_assembly_genomic.fna.gz
+# gzip -d GCA_900275575.1_Aplysina21_polished_assembly_genomic.fna.gz
+# Ephydatia muelleri
+# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/013/339/895/GCA_013339895.1_Emu_genome_v1/GCA_013339895.1_Emu_genome_v1_genomic.fna.gz
+# gzip -d GCA_013339895.1_Emu_genome_v1_genomic.fna.gz
 
 
 # Now perform search on positive control (amphimedon queenslandica NCBI genome)
 # full path to genome: "${HOME}/marine_drugs/marine_drugs/data/external/GCF_000090795.1_v1.0_genomic.fna"
 workdir="${HOME}/marine_drugs/marine_drugs/data/external"
-controls=("GCF_000090795.1_v1.0_genomic.fna" "GCA_016292275.1_UQ_AmQuee_3_genomic.fna")
+controls=("GCF_000090795.1_v1.0_genomic.fna" "GCA_016292275.1_UQ_AmQuee_3_genomic.fna" "GCA_900275565.1_Aplysina16A_polished_assembly_genomic" "GCA_900275575.1_Aplysina21_polished_assembly_genomic" "GCA_013339895.1_Emu_genome_v1_genomic.fna")
 for control in ${controls[@]};do
     docker run --rm -u $(id -u) -v $workdir:/busco_wd \
             ezlabgva/busco:v5.beta.1_cv1 \
