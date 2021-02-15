9dd7b8489d7887bc303bebd6ea8a21721bd05c06
WiscEvan <erees@wisc.edu>
2021-02-15 07:56:25 -0600

    :bug: Update incorrect script path

--- marine_drugs/src/features/host-annotation/busco-make-summaries-master-table.sh
+++ marine_drugs/src/features/host-annotation/busco-make-summaries-master-table.sh
@@ -1,1 +1,1 @@
-python extract_busco_summaries.py --output ~/marine_drugs/marine_drugs/data/interim/host-annotation/busco-marker-assessment/busco_metrics.tsv
+python busco-extract-summaries.py --output ~/marine_drugs/marine_drugs/data/interim/host-annotation/busco-marker-assessment/busco_metrics.tsv
