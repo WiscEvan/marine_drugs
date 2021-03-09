51a43ca45258a39e58d6583f6c7206265c36ea95
Evan Rees <erees@wisc.edu>
2021-03-09 12:51:06 -0600

    :bug: Fix fragmentation metric operator

--- marine_drugs/src/visualization/merge-sponges-binning-and-annotations.py
+++ marine_drugs/src/visualization/merge-sponges-binning-and-annotations.py
@@ -129,7 +129,7 @@ def fragmentation_metric(df, quality_measure=0.50):
     lengths = 0
     for length in df.length.sort_values(ascending=False):
         lengths += length
-        if lengths > target_size:
+        if lengths >= target_size:
             return length
 
 
