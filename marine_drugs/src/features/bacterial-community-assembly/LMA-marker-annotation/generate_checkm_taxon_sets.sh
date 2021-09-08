709637b822a9d6afbd54258599b1289581508f8f
WiscEvan <erees@wisc.edu>
2021-09-08 14:04:08 -0500

    :art: Add post-processing step where hmm models are hmmpressed

--- marine_drugs/src/features/bacterial-community-assembly/LMA-marker-annotation/generate_checkm_taxon_sets.sh
+++ marine_drugs/src/features/bacterial-community-assembly/LMA-marker-annotation/generate_checkm_taxon_sets.sh
@@ -7,22 +7,42 @@
 
 SRC="/home/evan/marine_drugs/marine_drugs/src/features/bacterial-community-assembly/LMA-marker-annotation/retrieve_checkm_taxon_set_markers.py"
 CHECKM_HMM="/home/evan/miniconda3/envs/sponges/checkm_data/hmms/checkm.hmm"
-
+EXTERNAL="/home/evan/marine_drugs/marine_drugs/data/external"
 
 rank="phylum"
 taxon="Proteobacteria"
-outfile="/home/evan/marine_drugs/marine_drugs/data/external/${rank}_${taxon}_markers.txt"
-checkm taxon_set $rank $taxon $outfile
+outfile="${EXTERNAL}/${rank}_${taxon}_markers.txt"
+if [ ! -f $outfile ]
+then
+    checkm taxon_set $rank $taxon $outfile
+else
+    echo "$outfile found, skipping checkm taxon_set ..."
+fi
 python $SRC --taxon-set $outfile --outdir ${outfile/.txt/} --checkm-hmm $CHECKM_HMM
 
 rank="class"
 taxon="Alphaproteobacteria"
-outfile="/home/evan/marine_drugs/marine_drugs/data/external/${rank}_${taxon}_markers.txt"
-checkm taxon_set $rank $taxon $outfile
+outfile="${EXTERNAL}/${rank}_${taxon}_markers.txt"
+if [ ! -f $outfile ]
+then
+    checkm taxon_set $rank $taxon $outfile
+else
+    echo "$outfile found, skipping checkm taxon_set ..."
+fi
 python $SRC --taxon-set $outfile --outdir ${outfile/.txt/} --checkm-hmm $CHECKM_HMM
 
 rank="class"
 taxon="Gammaproteobacteria"
-outfile="/home/evan/marine_drugs/marine_drugs/data/external/${rank}_${taxon}_markers.txt"
-checkm taxon_set $rank $taxon $outfile
+outfile="${EXTERNAL}/${rank}_${taxon}_markers.txt"
+if [ ! -f $outfile ]
+then
+    checkm taxon_set $rank $taxon $outfile
+else
+    echo "$outfile found, skipping checkm taxon_set ..."
+fi
 python $SRC --taxon-set $outfile --outdir ${outfile/.txt/} --checkm-hmm $CHECKM_HMM
+
+### now press all generated/retrieved HMM models
+for hmm in `ls ${EXTERNAL}/*/*.hmm`;do
+    hmmpress -f $hmm
+done
